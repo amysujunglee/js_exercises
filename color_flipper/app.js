188419eb9b6a998c9dfe1511f99b2506f5afb25f
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-09-03 22:36:38 +0900

    Add files via upload

---
+++ color_flipper/app.js
@@ -0,0 +1,33 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const getRandomColorBtn = document.querySelector('#getRandomColorBtn');
+const randomColorHex = document.querySelector('#randomColorHex');
+
+getRandomColorBtn.addEventListener('click', () => {
+
+    let hexColor = '#';
+
+    for (i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    document.body.style.backgroundColor = hexColor;
+    randomColorHex.textContent = hexColor;
+
+});
+
+window.addEventListener('load', () => {
+
+    let hexColor = '#';
+
+    for (i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    document.body.style.backgroundColor = hexColor;
+    randomColorHex.textContent = hexColor;
+
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
