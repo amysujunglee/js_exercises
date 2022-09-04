a3634e462bcbcefec5f35f964714131cb2fd01aa
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-09-04 23:00:42 +0900

    Add files via upload

--- color_flipper/app.js
+++ color_flipper/app.js
@@ -2,32 +2,28 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const getRandomColorBtn = document.querySelector('#getRandomColorBtn');
 const randomColorHex = document.querySelector('#randomColorHex');
 
-getRandomColorBtn.addEventListener('click', () => {
-
+function getRandomHexcode() {
     let hexColor = '#';
 
     for (i = 0; i < 6; i++) {
         hexColor += hex[getRandomNumber()];
     }
-
-    document.body.style.backgroundColor = hexColor;
-    randomColorHex.textContent = hexColor;
-
-});
-
-window.addEventListener('load', () => {
-
-    let hexColor = '#';
-
-    for (i = 0; i < 6; i++) {
-        hexColor += hex[getRandomNumber()];
-    }
-
-    document.body.style.backgroundColor = hexColor;
-    randomColorHex.textContent = hexColor;
-
-});
+    return hexColor;
+}
 
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length);
 }
+
+function addContent() {
+    document.body.style.backgroundColor = getRandomHexcode();
+    randomColorHex.textContent = getRandomHexcode();
+}
+
+getRandomColorBtn.addEventListener('click', () => {
+    addContent();
+});
+
+window.addEventListener('load', () => {
+    addContent()
+});
