62c2d1b44ffb2ed0953f13af7dec0319f359cd3d
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-09-06 22:28:31 +0900

    Add files via upload

--- random_password_generator/app.js
+++ random_password_generator/app.js
@@ -1,29 +1,30 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-const getRandomColorBtn = document.querySelector('#getRandomColorBtn');
-const randomColorHex = document.querySelector('#randomColorHex');
+const character = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$'];
+const getRandomPasswordBtn = document.querySelector('#get-random-password-btn');
+const copyBtn = document.querySelector('#copy-btn');
+const randomPassword = document.querySelector('#random-password');
 
 function getRandomHexcode() {
-    let hexColor = '#';
+    let password = '';
 
-    for (i = 0; i < 6; i++) {
-        hexColor += hex[getRandomNumber()];
+    for (i = 0; i < 10; i++) {
+        password += character[getRandomNumber()];
     }
-    return hexColor;
+    return password;
 }
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * hex.length);
+    return Math.floor(Math.random() * character.length);
 }
 
 function addContent() {
-    document.body.style.backgroundColor = getRandomHexcode();
-    randomColorHex.textContent = getRandomHexcode();
+    randomPassword.textContent = getRandomHexcode();
 }
 
-getRandomColorBtn.addEventListener('click', () => {
+getRandomPasswordBtn.addEventListener('click', () => {
     addContent();
 });
 
-window.addEventListener('load', () => {
-    addContent()
-});
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(randomPassword.textContent);
+    alert(`copied '${randomPassword.textContent}'`)
+})
