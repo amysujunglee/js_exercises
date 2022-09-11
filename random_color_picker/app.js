9f18a706e9e06bd92d564087d6265520e94ba51b
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-09-11 22:45:29 +0900

    Add files via upload

--- random_color_picker/app.js
+++ random_color_picker/app.js
@@ -1,29 +1,29 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-const getRandomColorBtn = document.querySelector('#getRandomColorBtn');
-const randomColorHex = document.querySelector('#randomColorHex');
-
-function getRandomHexcode() {
-    let hexColor = '#';
-
-    for (i = 0; i < 6; i++) {
-        hexColor += hex[getRandomNumber()];
-    }
-    return hexColor;
-}
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const getRandomColorBtn = document.querySelector("#getRandomColorBtn");
+const randomColorHex = document.querySelector("#randomColorHex");
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * hex.length);
+  return Math.floor(Math.random() * hex.length);
 }
 
-function addContent() {
-    document.body.style.backgroundColor = getRandomHexcode();
-    randomColorHex.textContent = getRandomHexcode();
-}
+getRandomColorBtn.addEventListener("click", () => {
+  let hexColor = "#";
 
-getRandomColorBtn.addEventListener('click', () => {
-    addContent();
+  for (i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+
+  document.body.style.backgroundColor = hexColor;
+  randomColorHex.textContent = hexColor;
 });
 
-window.addEventListener('load', () => {
-    addContent()
+window.addEventListener("load", () => {
+  let hexColor = "#";
+
+  for (i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+
+  document.body.style.backgroundColor = hexColor;
+  randomColorHex.textContent = hexColor;
 });
