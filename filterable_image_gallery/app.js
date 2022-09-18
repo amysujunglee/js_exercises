11e771655299d474f5be562ec55506d26c7c4352
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-09-18 23:03:57 +0900

    Add files via upload

--- filterable_image_gallery/app.js
+++ filterable_image_gallery/app.js
@@ -8,14 +8,54 @@ btns.forEach(btn => {
         const category = e.currentTarget.classList;
 
         if (category.contains('icecream')) {
+            icecreamImgs.forEach(img => {
+                img.classList.add('show');
+                img.classList.remove('hide');
+            });
             chocolateImgs.forEach(img => {
                 img.classList.add('hide');
+                img.classList.remove('show');
+            });
+            fruitsImgs.forEach(img => {
+                img.classList.add('hide');
+                img.classList.remove('show');
+            });
+        } else if (category.contains('chocolate')) {
+            icecreamImgs.forEach(img => {
+                img.classList.add('hide');
+                img.classList.remove('show');
+            });
+            chocolateImgs.forEach(img => {
+                img.classList.add('show');
+                img.classList.remove('hide');
+            });
+            fruitsImgs.forEach(img => {
+                img.classList.add('hide');
+                img.classList.remove('show');
             });
         } else if (category.contains('fruits')) {
+            icecreamImgs.forEach(img => {
+                img.classList.add('hide');
+                img.classList.remove('show');
+            });
+            chocolateImgs.forEach(img => {
+                img.classList.add('hide');
+                img.classList.remove('show');
+            });
             fruitsImgs.forEach(img => {
-                img.classList.add('hide')
+                img.classList.add('show');
+                img.classList.remove('hide');
+            });
+        } else {
+            icecreamImgs.forEach(img => {
+                img.classList.remove('hide', 'show');
+            });
+            chocolateImgs.forEach(img => {
+                img.classList.remove('hide', 'show');
+            });
+            fruitsImgs.forEach(img => {
+                img.classList.remove('hide', 'show');
             });
         }
     });
 });
-
