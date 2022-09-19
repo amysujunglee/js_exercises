ed7e16917c83e0df64f3a9bf154e4276c5851645
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-09-19 20:26:19 +0900

    Add files via upload

--- filterable_image_gallery/app.js
+++ filterable_image_gallery/app.js
@@ -2,24 +2,31 @@ const btns = document.querySelectorAll('.btn');
 const icecreamImgs = document.querySelectorAll('.icecreamImg');
 const chocolateImgs = document.querySelectorAll('.chocolateImg');
 const fruitsImgs = document.querySelectorAll('.fruitsImg');
+const allImgs = document.querySelectorAll('img');
+
+console.log(allImgs);
 
 btns.forEach(btn => {
     btn.addEventListener('click', (e) => {
         const category = e.currentTarget.classList;
 
         if (category.contains('icecream')) {
-            icecreamImgs.forEach(img => {
-                img.classList.add('show');
-                img.classList.remove('hide');
+            if(allImgs.forEach( img => {
+                img.hasClass
             });
-            chocolateImgs.forEach(img => {
-                img.classList.add('hide');
-                img.classList.remove('show');
-            });
-            fruitsImgs.forEach(img => {
-                img.classList.add('hide');
-                img.classList.remove('show');
-            });
+            // icecreamImgs.forEach(img => {
+            //     img.classList.add('show');
+            //     img.classList.remove('hide');
+            // });
+            // chocolateImgs.forEach(img => {
+            //     img.classList.add('hide');
+            //     img.classList.remove('show');
+            // });
+            // fruitsImgs.forEach(img => {
+            //     img.classList.add('hide');
+            //     img.classList.remove('show');
+            // });
+
         } else if (category.contains('chocolate')) {
             icecreamImgs.forEach(img => {
                 img.classList.add('hide');
