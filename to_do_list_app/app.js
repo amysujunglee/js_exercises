99f4781435bb943623bf50cd0fedf00117c98f42
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-09-26 23:55:27 +0900

    Add files via upload

--- to_do_list_app/app.js
+++ to_do_list_app/app.js
@@ -1,2 +1,13 @@
 const addTodo = document.querySelector('#add-todo');
 const addBtn = document.querySelector('#add-btn');
+const resetBtn = document.querySelector('#reset-btn');
+const todoList = document.querySelector('.todo-list')
+
+// Add a new todo
+addBtn.addEventListener('click', () => {
+    let newTodo = addTodo.value;
+    todoList.innerHTML +=
+        `<li class="todo-item">${newTodo}<button>Edit</button><button>X</button></li>`
+
+    newTodo.value = 'hello';
+});
