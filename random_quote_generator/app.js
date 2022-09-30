cb78efca5928fa2bb50dff15c890da0d01c7f8aa
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-09-30 07:28:46 -0400

    Add files via upload

--- random_quote_generator/app.js
+++ random_quote_generator/app.js
@@ -44,19 +44,24 @@ const quotes = [
 const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const getRandomQuoteBtn = document.querySelector('#get-random-quote-btn');
-const copyBtn = document.querySelector('#copyBtn');
+const copyBtn = document.querySelector('#copy-btn');
 
-// get random quote
-const getRandomQuote = () => {
-    let randomNum = 0;
-    randomNum = Math.floor(Math.random() * quotes.length + 1);
+// show random quote
+const showRandomQuote = () => {
+    let randomIndex = 0;
+    randomIndex = Math.floor(Math.random() * quotes.length);
+    quote.textContent = `"${quotes[randomIndex].quote}"`;
+    author.textContent = `- ${quotes[randomIndex].author} -`;
+}
 
-    quote.textContent = `"${quotes[randomNum].quote}"`;
-    author.textContent = `- ${quotes[randomNum].author} -`;
-};
-
-getRandomQuote();
+showRandomQuote();
 
 getRandomQuoteBtn.addEventListener('click', () => {
-    getRandomQuote();
+    showRandomQuote();
 });
+
+// copy quote
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText('hello');
+    alert(`You copied: ${quote.textContent} ${author.textContent}`)
+})
