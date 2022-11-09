3236175417588c7932303d31ce71cfb9c7136703
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-11-09 08:49:21 -0500

    Add files via upload

--- tip_calculator/app.js
+++ tip_calculator/app.js
@@ -8,10 +8,10 @@ form.addEventListener('submit', calculate);
 
 function calculate(e) {
     const total = bill.value * (tip.value / 100 + 1);
-    totalInput.value = total;
+    totalInput.value = total.toFixed(2);
 
     const totalTip = bill.value * (tip.value / 100);
-    totalTipInput.value = totalTip;
+    totalTipInput.value = totalTip.toFixed(2);
 
     e.preventDefault();
 }
