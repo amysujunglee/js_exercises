31041dea72a201b78dd938b03bd2455c6bb59bcd
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2022-11-10 07:11:15 -0500

    Add files via upload

--- number_guesser/app.js
+++ number_guesser/app.js
@@ -8,7 +8,7 @@
 
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 const game = document.querySelector('#game'),
@@ -29,15 +29,51 @@ guessBtn.addEventListener('click', () => {
     }
 
     if (guess === winningNum) {
-        guessInput.disabled = true;
-        guessInput.style.borderColor = 'green';
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+        // guessInput.disabled = true;
+        // guessInput.style.borderColor = 'green';
+        // setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+        gameOver(true, `${winningNum} is correct, YOU WIN!`);
     } else {
+        guessesLeft -= 1;
 
+        if (guessesLeft === 0) {
+            // guessInput.disabled = true;
+            // guessInput.style.borderColor = 'red';
+            // setMessage(`Game Over, you lost. The correct number was ${winningNum}.`, 'red');
+            gameOver(false, `Game Over, you lost. The correct number was ${winningNum}.`);
+        } else {
+            guessInput.style.borderColor = 'red';
+            guessInput.value = '';
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left.`, 'red')
+        }
     }
 })
+
+game.addEventListener('mousedown', e => {
+    if (e.target.className === 'play-again') {
+        window.location.reload();
+    }
+});
+
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function setMessage(msg, color) {
     message.textContent = msg;
     message.style.color = color;
+}
+
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    guessInput.disabled = true;
+    guessInput.style.borderColor = color;
+    message.style.color = color;
+    setMessage(msg);
+
+    // Play Again?
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
 }
